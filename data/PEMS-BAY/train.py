c97102552070a4f931199b11c3f427a7f181392d
changhaomeng111 <50974887+changhaomeng111@users.noreply.github.com>
2023-10-27 23:52:08 +0800

    Update train.py

--- data/PEMS-BAY/train.py
+++ data/PEMS-BAY/train.py
@@ -9,13 +9,14 @@ import numpy as np
 def main(args):
         with open(args.config_filename) as f:
             supervisor_config = yaml.safe_load(f)
-            adj_mx =  np.mat(np.load("./data/PEMS-BAY/OLA64.npy"))
+            LA =  np.mat(np.load("./data/PEMS-BAY/OLA64.npy"))
+            LA[LA<0]=0
             config = tf.ConfigProto(allow_soft_placement=True)
             os.environ["CUDA_VISIBLE_DEVICES"] = '3'
             config.gpu_options.allow_growth = False  # 程序按需申请内存
 
             with tf.Session(config=config) as sess:
-                supervisor = MGSTCNSupervisor(adj_mx=adj_mx, **supervisor_config)
+                supervisor = MGSTCNSupervisor(adj_mx=LA, **supervisor_config)
                 supervisor.train(sess=sess)
 
 if __name__ == '__main__':
